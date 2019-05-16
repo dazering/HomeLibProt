3e3e69a73202a81611d22ac4f708d444b043ae94
dazering <dazering@yandex.ru>
2019-05-16 21:06:21 +0300

    Add DataBase Model

--- HomeLib/Models/Book.cs
+++ HomeLib/Models/Book.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HomeLib.Models
 {
     public class Book
     {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Annotation { get; set; }
+        public string Year { get; set; }
+        public string Isbn { get; set; }
+        public string Cover { get; set; }
+
+        public Authtor Authtor { get; set; }
     }
 }
---
+++ HomeLib/Models/LibraryContex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeLib.Models
+{
+    public class LibraryContex : DbContext
+    {
+        public LibraryContex(DbContextOptions<LibraryContex> contextOptions) : base(contextOptions)
+        {
+
+        }
+
+        public DbSet<Authtor> Authtors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Authtor>().HasKey(a => new { a.FirstName, a.LastName });
+        }
+    }
+}
