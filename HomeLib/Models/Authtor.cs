b84c0343f9abd5e9121ab9975077ab4149ab9e91
dazering <47257427+dazering@users.noreply.github.com>
2019-05-16 21:06:21 +0300

    Add DataBase Model

---
+++ HomeLib/Models/Authtor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace HomeLib.Models
+{
+    public class Authtor
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
