f55a2dc8ccbb578dd147b8152a3ec273012de1c8
dazering <dazering@yandex.ru>
2019-06-03 12:15:45 +0300

    Added scanner and downloader

---
+++ HomeLib/Infrostructure/Scanner/ServiceProviderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using HomeLib.Models.Repository;
+
+namespace HomeLib.Infrostructure.Scanner
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void AddScannerService(this IServiceCollection services, string path)
+        {
+            services.AddSingleton<Scanner>(provider =>new Scanner(path,provider));
+        }
+        public static void AddDownloaderService(this IServiceCollection services, string path)
+        {
+            services.AddTransient<Downloader>(provider => new Downloader(path));
+        }
+    }
+}
---
+++ HomeLib/Infrostructure/Scanner/Status.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeLib.Infrostructure.Scanner
+{
+    public enum StateScanner
+    {
+        Unscanned,
+        Canceled,
+        InProgress,
+        Scanned,
+        Error
+    }
+    public class Status
+    {
+        public StateScanner StateScanner { get; set; }
+        private int countBookInLocalRepository = 0;
+        private int countBookInDataBase = 0;
+        public int CountBookInLocalRepository { get => countBookInLocalRepository; set => countBookInLocalRepository = value; }
+        public int CountBookInDataBase { get => countBookInDataBase; private set => countBookInDataBase = value; }
+        public void UpdateCountBookInDb(int sum)
+        {
+            CountBookInDataBase = sum;
+        }
+        private string timeElapsed;
+        public string TimeElapsed
+        {
+            get => timeElapsed;
+        }
+        public void GetTime(TimeSpan time)
+        {
+            timeElapsed = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", time.Hours, time.Minutes, time.Seconds, time.Milliseconds / 10);
+        }
+    }
+}
