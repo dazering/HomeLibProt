09dfafa8e778b0a1cbe27c166727db782c3ca4f5
dazering <47257427+dazering@users.noreply.github.com>
2019-12-20 19:00:40 +0300

    Added default value to names of Author

--- HomeLibServices/Models/Author.cs
+++ HomeLibServices/Models/Author.cs
@@ -5,9 +5,9 @@ namespace HomeLibServices.Models
     public class Author
     {
         public long AuthorId { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName { get; set; } = "Unknown";
+        public string MiddleName { get; set; } = "Unknown";
+        public string LastName { get; set; } = "Unknown";
         public string FullName { get; set; }
         public IEnumerable<Book> Books { get; set; }
     }
