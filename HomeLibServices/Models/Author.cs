f605d1eb72cec61b3173c9a94ea844e8c19b87db
dazering <47257427+dazering@users.noreply.github.com>
2019-12-20 18:42:15 +0300

    Added prop FullName

--- HomeLibServices/Models/Author.cs
+++ HomeLibServices/Models/Author.cs
@@ -8,6 +8,7 @@ namespace HomeLibServices.Models
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
+        public string FullName { get; set; }
         public IEnumerable<Book> Books { get; set; }
     }
 }
