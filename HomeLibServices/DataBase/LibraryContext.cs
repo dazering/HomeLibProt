89eba543dce933d2818cc273486704c4eba3a30e
dazering <dazering@yandex.ru>
2019-12-20 19:19:58 +0300

    Added context for db

---
+++ HomeLibServices/DataBase/LibraryContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HomeLibServices.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeLibServices.DataBase
+{
+   public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> cntx):base(cntx)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasAlternateKey(a => new {a.FirstName, a.MiddleName, a.LastName});
+            modelBuilder.Entity<Author>().Property(a => a.MiddleName).HasDefaultValue("");
+            modelBuilder.Entity<Author>().HasIndex(a => a.FullName);
+
+            modelBuilder.Entity<Book>().HasOne(b => b.Author).WithMany(a => a.Books).OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<Book>().Property(b => b.Title).IsRequired();
+            modelBuilder.Entity<Book>().HasIndex(b => b.Title);
+        }
+    }
+}
