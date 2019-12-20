585c09a8fb16c00b58c76fdba314828f69e79ed9
dazering <dazering@yandex.ru>
2019-12-20 17:45:37 +0300

    Added Book, Author and Path classes

---
+++ HomeLibServices/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeLibServices.Models
+{
+    public class Book
+    {
+        public long BookId { get; set; }
+        public string Title { get; set; }
+        public string Isbn { get; set; }
+        /// <summary>
+        /// Books cover in Base64 as string
+        /// </summary>
+        public string Cover { get; set; }
+
+        public string Annotation { get; set; }
+
+        public LocalPath Path { get; set; }
+        public Author Author { get; set; }
+
+        public Book()
+        {
+            Author = new Author();
+            Path = new LocalPath();
+        }
+    }
+}
---
+++ HomeLibServices/Models/LocalPath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeLibServices.Models
+{
+    public class LocalPath
+    {
+        /// <summary>
+        /// The file
+        /// </summary>
+        public Book Book { get; set; }
+        /// <summary>
+        /// Name of archive containing the file
+        /// </summary>
+        public string ArchiveName { get; set; }
+        /// <summary>
+        /// Name of the file
+        /// </summary>
+        public string FbName { get; set; }
+    }
+}
