b8b37b33e5c63d163a39af3c7cc9d9c7307fd976
dazering <dazering@yandex.ru>
2020-01-16 10:41:12 +0300

    Added new classes: event args and state of scanner

---
+++ HomeLibServices/Models/ScannerEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeLibServices.Models
+{
+    public class ScannerEventArgs : EventArgs
+    {
+        public ScannerState ScannerState { get; set; }
+        public ScannerEventArgs(ScannerState scanner)
+        {
+            ScannerState = scanner;
+        }
+    }
+}
---
+++ HomeLibServices/Models/ScannerState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HomeLibServices.Models
+{
+    public class ScannerState
+    {
+        public ScannerState(int inDb, int inLocalRepo)
+        {
+            BooksInDataBase = inDb;
+            BooksInLocalRepository = inLocalRepo;
+        }
+
+        public int CurrentErrorsCount { get; set; }
+        public int BooksInLocalRepository { get; set; }
+        public int BooksInDataBase { get; set; }
+        public int BooksNotAddedInDb { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime FinishTime { get; set; }
+        public TimeSpan ElapsedTime { get; set; }
+        public bool IsScanningRun { get; set; } = false;
+    }
+}
