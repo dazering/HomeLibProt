13124c3b693ba820e33d098b59dc2169b5173a14
dazering <dazering@yandex.ru>
2020-01-16 10:41:48 +0300

    Added scanner and logger

---
+++ HomeLibServices/Logger/ILogger.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HomeLibServices.Logger
+{
+    public interface ILogger
+    {
+        void WriteError(Exception exception, string archiveName, string fileName);
+    }
+}
---
+++ HomeLibServices/Logger/LocalLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace HomeLibServices.Logger
+{
+    public class LocalLogger : ILogger
+    {
+        private readonly string pathToLogDir;
+
+        public LocalLogger()
+        {
+            pathToLogDir = Path.Combine(Directory.GetCurrentDirectory(), "Logs");
+            if (!Directory.Exists(pathToLogDir))
+            {
+                Directory.CreateDirectory(pathToLogDir);
+            }
+        }
+
+        public void WriteError(Exception exception, string archiveName, string fileName)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(pathToLogDir, $"{DateTime.Now:yy-MM-dd}.txt"), true))
+            {
+                sw.WriteLine($"{DateTime.Now:T} {exception.GetType()} {archiveName} {fileName} {exception.Message}");
+            }
+        }
+    }
+}
