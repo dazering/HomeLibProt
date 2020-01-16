a003c1f0e5efbf828e92b14d7c7a16bee21245fe
dazering <47257427+dazering@users.noreply.github.com>
2020-01-16 11:06:34 +0300

    Removed spaces, unnecessery usings

--- HomeLibServices/ExtensionsMethods/StringExtensionsMethods.cs
+++ HomeLibServices/ExtensionsMethods/StringExtensionsMethods.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace HomeLibServices.ExtensionsMethods
+﻿namespace HomeLibServices.ExtensionsMethods
 {
     internal static class StringExtensionsMethods
     {
