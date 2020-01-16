ce4492933ea5ab9a691b3345e2e3eb8133e03605
dazering <47257427+dazering@users.noreply.github.com>
2020-01-16 11:49:19 +0300

    Added

---
+++ HomeLibServices/Managers/Downloader.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace HomeLibServices.Managers
+{
+    internal class Downloader
+    {
+        private readonly string pathToLocalRepository;
+
+        public Downloader(string path)
+        {
+            pathToLocalRepository = path;
+        }
+
+        internal byte[] ReadArchive(string archiveName, string fileName)
+        {
+            using (FileStream zipStream = File.OpenRead(Path.Combine(pathToLocalRepository, archiveName)))
+            using (ZipArchive zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Read))
+            {
+                ZipArchiveEntry zipArchiveEntry = zipArchive.Entries.FirstOrDefault(e => e.Name == fileName);
+                using (var stream = zipArchiveEntry.Open())
+                using (var ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    byte[] bytesOfBook = ms.ToArray();
+                    return bytesOfBook;
+                }
+            }
+        }
+    }
+}
