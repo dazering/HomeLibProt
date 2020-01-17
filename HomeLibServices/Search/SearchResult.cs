98c27343bf55e1a5defd5fffdeaca0de0381d726
dazering <47257427+dazering@users.noreply.github.com>
2020-01-17 19:28:02 +0300

    Added repository controls

---
+++ HomeLibServices/Search/SearchResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace HomeLibServices.Search
+{
+    internal class SearchResult<T> : List<T>
+    {
+        public SearchResult(IQueryable<T> query, string property, string searchTerm)
+        {
+            query = Search(query, property, searchTerm);
+            AddRange(query);
+        }
+
+        private static IQueryable<T> Search(IQueryable<T> query, string searchProperty, string searchTerm)
+        {
+            var parameter = Expression.Parameter(typeof(T), "x");
+            var source = searchProperty.Split('.').Aggregate((Expression)parameter, Expression.Property);
+            var body = Expression.Call(source, "Contains", Type.EmptyTypes, Expression.Constant(searchTerm, typeof(string)));
+            var lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
+            return query.Where(lambda);
+        }
+    }
+}
