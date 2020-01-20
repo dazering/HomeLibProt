0a944a1ff55f93a8035325a85114730e6f0e8cbb
dazering <dazering@yandex.ru>
2020-01-20 16:09:13 +0300

    Integrated new library

--- HomeLib/Controllers/StatusController.cs
+++ HomeLib/Controllers/StatusController.cs
@@ -1,39 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using HomeLibServices.Managers;
 using Microsoft.AspNetCore.Mvc;
-using HomeLib.Infrostructure.Scanner;
+
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace HomeLib.Controllers
 {
     public class StatusController : Controller
     {
-        Scanner Scanner;
-        public StatusController(Scanner scanner)
+        private BookManager BookManager;
+        public StatusController(BookManager manager)
         {
-            Scanner = scanner;
-        }
-        [Route("Status/StartScan")]
-        public IActionResult Scan()
-        {
-            Scanner.StartScanAsync();
-            return RedirectToAction(nameof(GetStatus));
+            BookManager = manager;
         }
 
         [Route("Status")]
         public IActionResult GetStatus()
         {
-            Status status = Scanner.GetStatus();
-            return View("Status", status);
-        }
-
-        [Route("Status/CancelScanning")]
-        public IActionResult CancelScanning()
-        {
-            Scanner.CancelScanning();
-            return RedirectToAction(nameof(GetStatus));
+            return View("Status", BookManager.GetScannerState());
         }
     }
 }
