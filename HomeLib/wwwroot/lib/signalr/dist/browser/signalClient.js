85cc9bb0c655daac3c634dc1d40f455b5623ba67
dazering <47257427+dazering@users.noreply.github.com>
2020-01-20 16:11:49 +0300

    Added client side scripts

---
+++ HomeLib/wwwroot/lib/signalr/dist/browser/signalClient.js
@@ -0,0 +1,33 @@
+﻿"use strict"
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/scanhub").build();
+
+const countLocalRepoLabel = document.getElementById("countLocalRepo");
+const countInDb = document.getElementById("countInDb");
+const notAddedBooks = document.getElementById("notAddedBooks");
+const isScanning = document.getElementById("isScanning");
+const elapsedTime = document.getElementById("elapsedTime");
+const startTime = document.getElementById("startTime");
+const startScan = document.getElementById("startBtn");
+const cancelScaning = document.getElementById("cancelBtn");
+
+connection.on("getStatus", function(status) {
+        countLocalRepoLabel.innerText = status.booksInLocalRepository;
+        countInDb.innerText = status.booksInDataBase;
+        notAddedBooks.innerText = status.booksNotAddedInDb;
+        isScanning.innerText = status.isScanningRun ? "In Progresssss" : "Not runnig";
+    elapsedTime.innerText = status.elapsedTime;
+    startTime.innerText = status.startTime;
+});
+
+startScan.addEventListener("click", function(event) {
+    connection.invoke("StartScanning");
+    cancelScaning.classList.toggle("disabled");
+});
+
+cancelScaning.addEventListener("click",
+    function(event) {
+        connection.invoke("StopScanning");
+        cancelScaning.classList.toggle("disabled");
+    });
+connection.start();
