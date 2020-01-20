d6f0e19ef82bbe3d06135d1f3531851aba889a86
dazering <47257427+dazering@users.noreply.github.com>
2020-01-20 16:09:53 +0300

    Added signalR Hub and Messanger

--- HomeLib/Infrostructure/Scanner/ScannerHub.cs
+++ HomeLib/Infrostructure/Scanner/ScannerHub.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HomeLibServices.Managers;
+using HomeLibServices.Models;
 using Microsoft.AspNetCore.SignalR;
 
 namespace HomeLib.Infrostructure.Scanner
 {
     public class ScannerHub : Hub
     {
-        public void Send(Scanner scanner)
+        private BookManager BookManager;
+
+        public ScannerHub(BookManager manager)
         {
-            
+            BookManager = manager;
+        }
+
+        public void StopScanning()
+        {
+            BookManager.StopUpdatingDbRepository();
+        }
+
+        public void StartScanning()
+        {
+            BookManager.StartUpdateDbRepository();
         }
     }
 }
---
+++ HomeLib/Infrostructure/Scanner/ScannerMessanger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeLibServices.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace HomeLib.Infrostructure.Scanner
+{
+    public class ScannerMessanger
+    {
+        private IHubContext<ScannerHub> hubContext;
+
+        public ScannerMessanger(IHubContext<ScannerHub> cntx)
+        {
+            hubContext = cntx;
+        }
+
+        public void SendMessage(object o, ScannerEventArgs e)
+        {
+            hubContext.Clients.All.SendAsync("getStatus", e.ScannerState);
+        }
+    }
+}
