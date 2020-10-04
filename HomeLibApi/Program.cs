da26b6178813e2f265cb997c0580a6da09ce4935
dazering <47257427+dazering@users.noreply.github.com>
2020-10-04 18:00:14 +0300

    Added new project

---
+++ HomeLibApi/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace HomeLibApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return new WebHostBuilder().UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory()).ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    IHostingEnvironment env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    if (env.IsDevelopment())
+                    {
+                        config.AddUserSecrets<Program>();
+                    }
+                }).UseIISIntegration().UseDefaultServiceProvider((context, options) =>
+                {
+                    options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                }).UseStartup<Startup>();
+        }
+    }
+}
