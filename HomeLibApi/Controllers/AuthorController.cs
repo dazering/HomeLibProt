da371f8e09b69f771fd1ce596d631257a5db2a53
dazering <47257427+dazering@users.noreply.github.com>
2020-10-05 20:56:31 +0300

    Added new controllers

---
+++ HomeLibApi/Controllers/AuthorController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeLibServices.Managers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeLibApi.Controllers
+{
+    [Route("authors")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        BookManager BookManager;
+
+        public AuthorController(BookManager bookManager)
+        {
+            BookManager = bookManager;
+        }
+
+        [Route("author/{id}")]
+        public IActionResult GetBooksByAuthorId(long id)
+        {
+            try
+            {
+                return Ok(BookManager.GetAuthor(id));
+            }
+            catch (SqlException)
+            {
+                return StatusCode(500, "Сервер базы данных не доступен");
+            }
+        }
+
+        [Route("{literals}")]
+        public IActionResult SearchFirstLiteralsOfAuthorName(string literals = "")
+        {
+            try
+            {
+                var authors = BookManager.GetAuthorsFirstLiteral(literals);
+                if (authors != null)
+                {
+                    return Ok(authors);
+                }
+
+                return NotFound("Автор не найден");
+            }
+            catch (SqlException)
+            {
+                return StatusCode(500, "Сервер базы данных не доступен");
+            }
+        }
+
+        [Route("getauthors/{name}")]
+        public IActionResult GetAuthorsByFirstLiterals(string name)
+        {
+            try
+            {
+                return Ok(BookManager.SearchAuthorsByFirstLiteral(name));
+            }
+            catch (SqlException)
+            {
+                return StatusCode(500, "Сервер базы данных не доступен");
+            }
+        }
+
+        [Route("search/{name}")]
+        public IActionResult SearchAuthor(string name)
+        {
+            try
+            {
+                return Ok(BookManager.SearchAuthorsByName(name));
+            }
+            catch (SqlException)
+            {
+                return StatusCode(500, "Сервер базы данных не доступен");
+            }
+        }
+    }
+}
---
+++ HomeLibApi/Controllers/BookController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using HomeLibServices.Managers;
+using HomeLibServices.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeLibApi.Controllers
+{
+    [Route("books")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        BookManager BookManager;
+
+        public BookController(BookManager bookManager)
+        {
+            BookManager = bookManager;
+        }
+
+        [Route("search/{title}")]
+        public IActionResult SearchBook(string title)
+        {
+            return Ok(BookManager.SearchBooksByTitle(title));
+        }
+
+        [Route("book/{bookId}")]
+        public IActionResult GetBook(long bookId)
+        {
+            Book book;
+            try
+            {
+                book = BookManager.GetBook(bookId);
+            }
+            catch (SqlException)
+            {
+                return StatusCode(500, "Сервер базы данных не доступен");
+            }
+            if (book != null)
+            {
+                return Ok(book);
+            }
+
+            return NotFound("Книга не найдена");
+        }
+
+        [Route("Download/{id}")]
+        public IActionResult GetFile(long id)
+        {
+            Book book;
+            try
+            {
+                book = BookManager.GetBook(id);
+            }
+            catch (FileNotFoundException)
+            {
+                return NotFound("Файл не найден");
+            }
+            catch (SqlException)
+            {
+                return StatusCode(500,"Сервер базы данных не доступен.");
+            }
+            if (book != null)
+            {
+                return File(BookManager.GetBookBytes(book), "application/x-fictionbook", $"{book.Title}.fb2");
+            }
+            return NotFound("Книга не найдена");
+        }
+    }
+}
