8f088c5ac49c35cd751dc0ca977a78edc8f09ac9
dazering <dazering@yandex.ru>
2020-11-03 14:32:48 +0300

    Added new component: main

---
+++ HomeLibReactClient/reactclient/src/components/main/main.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import AlphabetSearchBlock from "./alphabetSearchBlock/alphabetSearchBlock";
+import AuthorsBlock from "./authorsBlock/authorsBlock";
+import BookInfoBlock from "./bookInfoBlock/bookInfoBlock";
+import BooksBlock from "./booksBlock/booksBlock";
+
+const main = () => {
+  return (
+    <main className="bg-secondary p-1 rounded h-auto my-3">
+      <Switch>
+        <Route path="/authors:name?" component={AuthorsBlock}></Route>
+        <Route path="/books/:authorId" component={BooksBlock}></Route>
+        <Route path="/book/:bookId" component={BookInfoBlock}></Route>
+        <Route path="/:literals?" exact component={AlphabetSearchBlock}></Route>
+        <Redirect to="/"></Redirect>
+      </Switch>
+    </main>
+  );
+};
+
+export default main;
