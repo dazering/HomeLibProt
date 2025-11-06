89ca3322c35001710b010abe5cd699d56217eb2b
dazering <dazering@yandex.ru>
2025-11-06 11:50:34 +0300

    Get rid of HomeLibReactClient

--- HomeLibReactClient/reactclient/src/components/main/errorMesssage/errorMessage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import './errorMessage.css'
-const ErrorMessage = (props) => (
-  <div className="bg-warning error text-center">{props.message}</div>
-);
-
-export default ErrorMessage;
