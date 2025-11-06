4f4d299705746c215cd153ce50495612d900e8a5
dazering <dazering@yandex.ru>
2025-11-06 19:00:01 +0300

    Add Authors

---
+++ HomeLibProt.Domain/DataAccess/Authors.cs
@@ -0,0 +1,37 @@
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace HomeLibProt.Domain.DataAccess;
+
+public record Author(long Id, string Name);
+
+public record AuthorParam(string FullName, string LastName, string FirstName, string MiddleName);
+
+public static class Authors {
+    public static async Task<Author[]> GetAuthorsByNameAsync(DbConnection connection, string[] authorNames) {
+        var sql =
+            @"
+select Id, FullName as Name from Authors
+where FullName in @Names
+";
+
+        var authors = await connection.QueryAsync<Author>(sql, new { Names = authorNames });
+
+        return authors.ToArray();
+    }
+
+    public static async Task InsertAuthorsAsync(DbConnection connection, AuthorParam[] authorNames) {
+        var sql =
+            @"
+insert into
+Authors (FullName, FirstName, MiddleName, LastName)
+values (@FullName, @FirstName, @MiddleName, @LastName)
+";
+
+        foreach (var a in authorNames) {
+            var _ = await connection.ExecuteAsync(sql, a);
+        }
+    }
+}
