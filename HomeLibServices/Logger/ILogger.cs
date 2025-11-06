e69e6751cf5dc6b4b8a3fd31b2c647ae27e234f0
dazering <dazering@yandex.ru>
2025-11-06 11:49:32 +0300

    Get rid of HomeLibServices

--- HomeLibServices/Logger/ILogger.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace HomeLibServices.Logger
-{
-    public interface ILogger
-    {
-        void WriteError(Exception exception, string archiveName, string fileName);
-    }
-}
--- HomeLibServices/Models/AlphabetSearchResult.cs
+++
@@ -1,8 +0,0 @@
-namespace HomeLibServices.Models
-{
-    public class AlphabetSearchResult
-    {
-        public int Count { get; set; }
-        public string Alphabets { get; set; }
-    }
-}
