c9f54d3bd581f8f2086b1d949d1be8854a5a241f
dazering <dazering@yandex.ru>
2025-11-07 15:12:11 +0300

    Add Genres tests

---
+++ HomeLibProt.Domain.Tests/DataAccess/TestGenres.cs
@@ -0,0 +1,42 @@
+using System.Threading.Tasks;
+using HomeLibProt.Domain.DataAccess;
+using HomeLibProt.Domain.Tests.Entities;
+using HomeLibProt.Domain.Tests.Utils;
+
+namespace HomeLibProt.Domain.Tests.DataAccess;
+
+public class TestGenres {
+    [Test]
+    public async Task TestGetGenresByKeyAsync() {
+        var expected = new[] { new Genre(Id: 2, Key: "genre2") };
+
+        var actual = await TestUtils.UseTestDatabase(async (connection) => {
+            await ConnectionUtils.DoInTransactionAsync(connection, GenreUtils.SetUpTestData);
+            return await ConnectionUtils.DoInTransactionAsync(connection, async (c) => {
+                return await Genres.GetGenresByKeyAsync(c, ["genre2", "genre3"]);
+            });
+        });
+
+        Assert.That(actual, Is.EqualTo(expected).AsCollection);
+    }
+
+    [Test]
+    public async Task TestInsertGenresAsync() {
+        var expected = new[] {
+            new TestGenre(Id: 1, Key: "genre1", Name: "Genre 1"),
+            new TestGenre(Id: 2, Key: "genre2", Name: "Genre 2"),
+            new TestGenre(Id: 3, Key: "genre3", Name: null),
+        };
+
+        var actual = await TestUtils.UseTestDatabase(async (connection) => {
+            await ConnectionUtils.DoInTransactionAsync(connection, GenreUtils.SetUpTestData);
+            await ConnectionUtils.DoInTransactionAsync(connection, async (c) => {
+                await Genres.InsertGenresAsync(c, ["genre3"]);
+            });
+
+            return await ConnectionUtils.DoInTransactionAsync(connection, GenreUtils.GetTestData);
+        });
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
