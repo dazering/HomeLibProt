2ad1adbe9a3f60f1f57f4e732f68a8b3886ff335
dazering <dazering@yandex.ru>
2025-11-07 16:56:45 +0300

    Add BookSeries

---
+++ HomeLibProt.Domain/DataAccess/BookSeries.cs
@@ -0,0 +1,19 @@
+using System.Data.Common;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace HomeLibProt.Domain.DataAccess;
+
+public record BookSeriesParam(long BookId, long SeriesId, long SeriesNumber);
+
+public static class BookSeries {
+    public static async Task InsertBookSeriesAsync(DbConnection connection, BookSeriesParam bookSeriesParam) {
+        var sql =
+            @"
+insert into
+BookSeries (BookId, SeriesId, SeriesNumber)
+values (@BookId, @SeriesId, @SeriesNumber)
+";
+            await connection.ExecuteAsync(sql, bookSeriesParam);
+    }
+}
