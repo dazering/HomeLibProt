25693f185422fc4d82190315a4022d5b1003cbe1
dazering <dazering@yandex.ru>
2025-11-11 20:56:19 +0300

    Add CollectionImporter

---
+++ HomeLibProt.CollectionImporter/CollectionImporter.fs
@@ -0,0 +1,41 @@
+module HomeLibProt.CollectionImporter.CollectionImporter
+
+open System.Data.Common
+open System.Threading.Tasks
+
+open HomeLibProt.Domain.DataAccess
+
+type CollectionImporterParameters =
+    { PathToInpx: string
+      BatchSize: int
+      MaxCountLeafs: int
+      DoInTransactionAsync: DbConnection * (DbConnection -> Task) -> Task }
+
+let importCollectionToDb (parameters: CollectionImporterParameters) (connection: DbConnection) : Task =
+    task {
+        do! (connection, DbStructure.CreateFullStructure) |> parameters.DoInTransactionAsync
+
+        let inpxImporterParameters: InpxImporter.InpxImporterParameters =
+            { PathToInpx = parameters.PathToInpx
+              BatchSize = parameters.BatchSize
+              DoInTransactionAsync = parameters.DoInTransactionAsync }
+
+        do! InpxImporter.importInpxToDb inpxImporterParameters connection
+
+        let ahsImporterParameters: AuthorHierarchicalSearchImporter.AuthorHierarchicalSearchImporterParameters =
+            { MaxCountLeafs = parameters.MaxCountLeafs
+              DoInTransactionAsync = parameters.DoInTransactionAsync }
+
+        do! AuthorHierarchicalSearchImporter.importAuthorHierarchicalSearchToDbAsync ahsImporterParameters connection
+    }
+
+let reimportAuthorHierarchicalSearch (parameters: CollectionImporterParameters) (connection: DbConnection) : Task =
+    task {
+        do! (connection, DbStructure.CreateAHSStructure) |> parameters.DoInTransactionAsync
+
+        let ahsImporterParameters: AuthorHierarchicalSearchImporter.AuthorHierarchicalSearchImporterParameters =
+            { MaxCountLeafs = parameters.MaxCountLeafs
+              DoInTransactionAsync = parameters.DoInTransactionAsync }
+
+        do! AuthorHierarchicalSearchImporter.importAuthorHierarchicalSearchToDbAsync ahsImporterParameters connection
+    }
