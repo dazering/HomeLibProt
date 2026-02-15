4a55e50303a24913d5216a9c4c66a0844e4fb885
dazering <dazering@yandex.ru>
2026-02-15 22:18:21 +0300

    Get rid of CollectionValidatorUtils

--- HomeLibProt.CollectionImporter.Tests/TestCollectionValidator.fs
+++ HomeLibProt.CollectionImporter.Tests/TestCollectionValidator.fs
@@ -17,10 +17,10 @@ let TestValidateCollectionAsync () =
     task {
         let expected =
             [| "Collection validation"
-               "Validating 000001-000001.zip"
-               "Absent files in 000001-000001.zip: 2.fb2"
-               "Validating 000002-000003.zip"
-               "Archive isn't exsists: 000002-000003.zip"
+               "Validating 000001-000002.zip"
+               "Absent files in 000001-000002.zip: 2.fb2"
+               "Validating 000003-000003.zip"
+               "Archive isn't exsists: 000003-000003.zip"
                "Collection validation finished" |]
 
         let actualMessages = Collections.Generic.List<string>()
@@ -35,7 +35,56 @@ let TestValidateCollectionAsync () =
         do!
             TestUtils.UseTestDatabase(fun connection ->
                 task {
-                    do! ConnectionUtils.DoInTransactionAsync(connection, CollectionValidatorUtils.setUpData)
+                    do!
+                        ConnectionUtils.DoInTransactionAsync(
+                            connection,
+                            fun c ->
+                                task {
+                                    let! _ =
+                                        BookUtils.Create(
+                                            c,
+                                            title = "Title1",
+                                            fileName = "1",
+                                            size = 1,
+                                            libId = "1",
+                                            deleted = false,
+                                            extension = "fb2",
+                                            date = "2025-11-07",
+                                            folder = "000001-000002.zip",
+                                            libRate = 0
+                                        )
+
+                                    let! _ =
+                                        BookUtils.Create(
+                                            c,
+                                            title = "Title2",
+                                            fileName = "2",
+                                            size = 1,
+                                            libId = "2",
+                                            deleted = false,
+                                            extension = "fb2",
+                                            date = "2025-11-07",
+                                            folder = "000001-000002.zip",
+                                            libRate = 0
+                                        )
+
+                                    let! _ =
+                                        BookUtils.Create(
+                                            c,
+                                            title = "Title3",
+                                            fileName = "3",
+                                            size = 1,
+                                            libId = "3",
+                                            deleted = false,
+                                            extension = "fb2",
+                                            date = "2025-11-07",
+                                            folder = "000003-000003.zip",
+                                            libRate = 0
+                                        )
+
+                                    do ()
+                                }
+                        )
 
                     do! validateCollectionAsync collectionValidatorParameters connection
                 })
