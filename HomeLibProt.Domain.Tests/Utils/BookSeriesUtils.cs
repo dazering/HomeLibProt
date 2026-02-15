e5780a3b915637589ba68ff548cb6a6aa47a26a5
dazering <dazering@yandex.ru>
2026-02-15 20:49:37 +0300

    Refactor BookSeriesUtils

--- HomeLibProt.Domain.Tests/Utils/BookSeriesUtils.cs
+++ HomeLibProt.Domain.Tests/Utils/BookSeriesUtils.cs
@@ -5,41 +5,6 @@ using HomeLibProt.Domain.Tests.Entities;
 namespace HomeLibProt.Domain.Tests.Utils;
 
 public static class BookSeriesUtils {
-    public static async Task SetUpTestData(DbConnection connection) {
-        var sql = @"
-insert into
-Books
-    (Id,
-    Title,
-    FileName,
-    Size,
-    LibId,
-    Deleted,
-    Extension,
-    Date,
-    Folder,
-    LibRate)
-values
-    (1,
-    'Title1',
-    'File1',
-    1,
-    'File1',
-    0,
-    'fb2',
-    '2025-11-7',
-    'archive1.zip',
-    0);
-
-insert into
-Series (Id, Name)
-values
-(1, 'Series 1')
-";
-
-        await TestUtils.InsertIntoTestDatabase(connection, sql);
-    }
-
     public static async Task<TestBookSeriesEntity[]> GetTestData(DbConnection connection) {
         var sql = @"
 select BookId, SeriesId, SeriesNumber from BookSeries
