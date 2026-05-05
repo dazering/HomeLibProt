6fc57823510500499ae3547fa4ba64b84f5a5139
dazering <dazering@yandex.ru>
2026-05-05 21:12:29 +0300

    Add ability to insert GenreEntity - tests

--- HomeLibProt.Domain.Tests/DataAccess/TestGenres.cs
+++ HomeLibProt.Domain.Tests/DataAccess/TestGenres.cs
@@ -47,4 +47,28 @@ public class TestGenres {
 
         Assert.That(actual, Is.EqualTo(expected));
     }
+
+    [Test]
+    public async Task TestInsertGenreEntityAsync() {
+        var expected = new[] {
+            new TestGenre(Id: 1, Key: "genre1", Name: "Genre 1"),
+            new TestGenre(Id: 2, Key: "genre2", Name: "Genre 2"),
+            new TestGenre(Id: 30, Key: "genre30", Name: "Genre 30"),
+        };
+
+        var actual = await TestUtils.UseTestDatabase(async (connection) => {
+            await ConnectionUtils.DoInTransactionAsync(connection, async (c) => {
+                await GenreUtils.Create(connection, key: "genre1", name: "Genre 1");
+                await GenreUtils.Create(connection, key: "genre2", name: "Genre 2");
+            });
+
+            await ConnectionUtils.DoInTransactionAsync(connection, async (c) => {
+                await Genres.InsertGenreEntityAsync(c, new GenreEntityParam(Id: 30, Key: "genre30", Name: "Genre 30"));
+            });
+
+            return await ConnectionUtils.DoInTransactionAsync(connection, GenreUtils.GetTestData);
+        });
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 }
