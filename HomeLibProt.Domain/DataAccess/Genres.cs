5268f98406aca51106aa250a65ea91c75a90a246
dazering <dazering@yandex.ru>
2026-05-05 21:10:16 +0300

    Add ability to insert GenreEntity

--- HomeLibProt.Domain/DataAccess/Genres.cs
+++ HomeLibProt.Domain/DataAccess/Genres.cs
@@ -7,6 +7,8 @@ namespace HomeLibProt.Domain.DataAccess;
 
 public record Genre(long Id, string Key);
 
+public record GenreEntityParam(long Id, string Key, string Name);
+
 public static class Genres {
     public static async Task<Genre[]> GetGenresByKeyAsync(DbConnection connection, string[] genreKeys) {
         var sql =
@@ -31,5 +33,16 @@ values (@Key)
         foreach (var k in genresKeys) {
             var _ = await connection.ExecuteAsync(sql, new { Key = k });
         }
+    }
+
+    public static async Task InsertGenreEntityAsync(DbConnection connection, GenreEntityParam genre) {
+        var sql =
+            @"
+insert into
+Genres (Id, Key, Name)
+values (@Id, @Key, @Name)
+";
+
+        await connection.ExecuteAsync(sql, genre);
     }
 }
