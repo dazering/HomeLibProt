579256dcece2e0d0b9a1266beed1877610b10bd0
dazering <dazering@yandex.ru>
2026-05-06 18:57:34 +0300

    Add RegExGroup names

---
+++ HomeLibProt.CollectionManager/RegEx/RegExGrops.fs
@@ -0,0 +1,43 @@
+module HomeLibProt.CollectionManager.RegEx.RegExGroups
+
+module Authors =
+    let authorId = "authorId"
+    let firstName = "firstName"
+    let middleName = "middleName"
+    let lastName = "lastName"
+
+module Authorships =
+    let authorId = "authorId"
+    let bookId = "bookId"
+
+module BookGenres =
+    let bookId = "bookId"
+    let genreId = "genreId"
+
+module Books =
+    let bookId = "bookId"
+    let fileSize = "fileSize"
+    let time = "time"
+    let title = "title"
+    let fileType = "fileType"
+    let deleted = "deleted"
+
+module BookSeries =
+    let bookId = "bookId"
+    let seriesId = "seriesId"
+    let seriesNumber = "seriesNumber"
+
+module Genres =
+    let genreId = "genreId"
+    let key = "key"
+    let name = "name"
+
+module Keywords =
+    let keywords = "keywords"
+
+module Languages =
+    let lang = "lang"
+
+module Series =
+    let seriesId = "seriesId"
+    let name = "name"
