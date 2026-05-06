ae9821b0b60afba7f2d9afbf2410c50c38cf18ec
dazering <dazering@yandex.ru>
2026-05-06 19:38:24 +0300

    Add sql dumps for Flibusta

---
+++ HomeLibProt.CollectionManager/SqlDumps/Flibusta.fs
@@ -0,0 +1,15 @@
+module HomeLibProt.CollectionManager.SqlDumps.Flibusta
+
+let authors = "lib.libavtorname.sql.gz"
+
+let authorships = "lib.libavtor.sql.gz"
+
+let bookGenres = "lib.libgenre.sql.gz"
+
+let books = "lib.libbook.sql.gz"
+
+let bookSeries = "lib.libseq.sql.gz"
+
+let genres = "lib.libgenrelist.sql.gz"
+
+let series = "lib.libseqname.sql.gz"
