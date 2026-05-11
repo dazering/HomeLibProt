0477309da15f6a868318bfea649a6c475502fa65
dazering <dazering@yandex.ru>
2026-05-11 21:50:16 +0300

    Add sql dumps for Librusec

---
+++ HomeLibProt.CollectionManager/SqlDumps/Librusec.fs
@@ -0,0 +1,15 @@
+module HomeLibProt.CollectionManager.SqlDumps.Librusec
+
+let authors = "libavtors.sql.gz"
+
+let authorships = "libavtor.sql.gz"
+
+let bookGenres = "libgenre.sql.gz"
+
+let books = "libbook.sql.gz"
+
+let bookSeries = "libseq.sql.gz"
+
+let genres = "libgenres.sql.gz"
+
+let series = "libseqs.sql.gz"
