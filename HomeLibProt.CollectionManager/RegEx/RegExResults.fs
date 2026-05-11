0b7a22b2c3ec871a224c4501ddfcad132e873742
dazering <dazering@yandex.ru>
2026-05-11 17:32:37 +0300

    Import Books

--- HomeLibProt.CollectionManager/RegEx/RegExResults.fs
+++ HomeLibProt.CollectionManager/RegEx/RegExResults.fs
@@ -2,6 +2,7 @@ module HomeLibProt.CollectionManager.RegEx.RegExResults
 
 open System
 open System.Text.RegularExpressions
+open System.Globalization
 
 type AuthorResult =
     { Id: int64
@@ -10,7 +11,38 @@ type AuthorResult =
       LastName: string
       FullName: string }
 
+type BookResult =
+    { Id: int64
+      FileSize: int64
+      Date: string
+      Title: string
+      Lang: string
+      Extension: string
+      Deleted: bool
+      Keywords: string array }
+
+let formatDate (dateTime: string) =
+    DateTime.ParseExact(dateTime, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture).ToString("yyyy-MM-dd")
+
+let private makeKeywords (keywords: string) : string array =
+    keywords.Split(',', StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
+
+let private convertStringToBool (value: string) : bool =
+    match value with
+    | "0" -> false
+    | _ -> true
+
 let private extractGroupValue (group: Group) : string = group.Value
+
+let getBookResult (groups: GroupCollection) : BookResult =
+    { Id = groups.[RegExGroups.Books.bookId] |> extractGroupValue |> int64
+      FileSize = groups.[RegExGroups.Books.fileSize] |> extractGroupValue |> int64
+      Date = groups.[RegExGroups.Books.time] |> extractGroupValue |> formatDate
+      Title = groups.[RegExGroups.Books.title] |> extractGroupValue
+      Lang = groups.[RegExGroups.Languages.lang] |> extractGroupValue
+      Extension = groups.[RegExGroups.Books.fileType] |> extractGroupValue
+      Deleted = groups.[RegExGroups.Books.deleted] |> extractGroupValue |> convertStringToBool
+      Keywords = groups.[RegExGroups.Keywords.keywords] |> extractGroupValue |> makeKeywords }
 
 let getAuthorResult (groups: GroupCollection) : AuthorResult =
     { Id = groups.[RegExGroups.Authors.authorId] |> extractGroupValue |> int64
--- HomeLibProt.CollectionManager/SqlDumps/SqlDumpImporter.fs
+++ HomeLibProt.CollectionManager/SqlDumps/SqlDumpImporter.fs
@@ -15,6 +15,73 @@ type SqlDumpImporterParameters =
       ProgressReport: string -> unit
       DoInTransactionAsync: DbConnection * (DbConnection -> Task) -> Task }
 
+let private archiveName = "SQL_Dump"
+
+let private bookResultToEntityParam (archiveId: int64) (languageId: int64) (book: BookResult) : BookEntityParam =
+    BookEntityParam(
+        Id = book.Id,
+        Title = book.Title,
+        FileName = $"{book.Id}",
+        Size = book.FileSize,
+        LibId = $"{book.Id}",
+        Deleted = book.Deleted,
+        Extension = book.Extension,
+        Date = book.Date,
+        ArchiveId = archiveId,
+        LibRate = 0,
+        LanguageId = languageId
+    )
+
+let importBookKeywords (bookId: int64) (keywords: string array) (connection: DbConnection) : Task<unit> =
+    task {
+        let! exsisted = Keywords.GetKeywordsByNameAsync(connection, keywords)
+
+        let bookKeywordParams =
+            exsisted
+            |> Array.map (fun k -> BookKeywordParam(BookId = bookId, KeywordId = k.Id))
+
+        do! BookKeywords.InsertBookKeywordsAsync(connection, bookKeywordParams)
+    }
+
+let importKeywords (keywords: string array) (connection: DbConnection) : Task<unit> =
+    task {
+        let! exsisted = Keywords.GetKeywordsByNameAsync(connection, keywords)
+
+        let map = exsisted |> Array.map (fun k -> k.Name, k.Id) |> Map
+
+        let keywordsToAdd =
+            keywords |> Array.filter (fun k -> map |> Map.containsKey k |> not)
+
+        do! Keywords.InsertKeywordsAsync(connection, keywordsToAdd)
+    }
+
+let private getLanguageId (lang: string) (connection: DbConnection) : Task<int64> =
+    task {
+        match! Languages.GetLanguagesByNameAsync(connection, [| lang |]) with
+        | [||] ->
+            do! Languages.InsertLanguagesAsync(connection, [| lang |])
+            let! ee = Languages.GetLanguagesByNameAsync(connection, [| lang |])
+
+            match ee with
+            | [| language |] -> return language.Id
+            | _ -> return! failwith $"Language '{lang}' not found"
+        | [| language |] -> return language.Id
+        | _ -> return! failwith $"There are more then one '{lang}'"
+
+    }
+
+let private importBookResult (archiveId: int64) (connection: DbConnection) (result: BookResult) : Task<unit> =
+    task {
+        let! languageId = getLanguageId result.Lang connection
+
+        let entityParam = result |> bookResultToEntityParam archiveId languageId
+
+        do! Books.InsertBookEntityAsync(connection, entityParam)
+
+        do! importKeywords result.Keywords connection
+        do! importBookKeywords result.Id result.Keywords connection
+    }
+
 let private authorResultToEntityParam (author: AuthorResult) : AuthorEntityParam =
     AuthorEntityParam(
         Id = author.Id,
@@ -49,11 +116,22 @@ let private importFromGZip
             do! importResult connection result
     }
 
+let getArchiveId (connection: DbConnection) : Task<int64> =
+    task {
+        do! Archives.InsertArchivesAsync(connection, [| archiveName |])
+
+        match! Archives.GetArchivesByNameAsync(connection, [| archiveName |]) with
+        | [| archive |] -> return archive.Id
+        | [||] -> return! failwith $"There is no archive with name: {archiveName}"
+        | _ -> return! failwith $"There are more then one archive with name: {archiveName}"
+    }
+
 let importSqlDumpsFlibustaAsync (parameters: SqlDumpImporterParameters) (connection: DbConnection) : Task<unit> =
     task {
         do! parameters.DoInTransactionAsync(connection, DbStructure.CreateImportSqlDumpStructure)
 
         let authors = Path.Combine(parameters.PathToSqlDumps, Flibusta.authors)
+        let books = Path.Combine(parameters.PathToSqlDumps, Flibusta.books)
 
         parameters.ProgressReport $"Importing: {Flibusta.authors}"
 
@@ -68,6 +146,25 @@ let importSqlDumpsFlibustaAsync (parameters: SqlDumpImporterParameters) (connect
                                 HomeLibProt.CollectionManager.RegEx.Flibusta.authors
                                 importAuthorResult
                                 getAuthorResult
+                                c
+                    }
+            )
+
+        parameters.ProgressReport $"Importing: {Flibusta.books}"
+
+        do!
+            parameters.DoInTransactionAsync(
+                connection,
+                fun (c: DbConnection) ->
+                    task {
+                        let! archiveId = getArchiveId c
+
+                        do!
+                            importFromGZip
+                                books
+                                HomeLibProt.CollectionManager.RegEx.Flibusta.books
+                                (importBookResult archiveId)
+                                getBookResult
                                 c
                     }
             )
