d34adaad7347927d7c333340a700ea4e2f0a50c8
dazering <dazering@yandex.ru>
2026-05-11 13:07:17 +0300

    Add Arguments

---
+++ HomeLibProt.CollectionManager/Arguments.fs
@@ -0,0 +1,26 @@
+﻿module HomeLibProt.CollectionManager.Arguments
+
+open Argu
+
+type Site =
+    | Flibusta = 1
+
+type CLIArguments =
+    | [<CliPrefix(CliPrefix.None)>] ImportSqlDumps of ParseResults<ImportSqlDumps>
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | ImportSqlDumps _ -> "Import inpx file to database."
+
+and ImportSqlDumps =
+    | [<ExactlyOnce; AltCommandLine("-i")>] PathToSqlDumps of string
+    | [<ExactlyOnce; AltCommandLine("-d")>] PathToDatabase of string
+    | [<ExactlyOnce; AltCommandLine("-s")>] Site of Site
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | PathToSqlDumps _ -> "Path to where save sql dumps on local file system"
+            | PathToDatabase _ -> "Path to database on local file system"
+            | Site _ -> "Source of sql dumps"
