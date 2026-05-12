bbcb576b7f3ac6a347d7595cd5fe1be7f2381838
dazering <dazering@yandex.ru>
2026-05-12 20:27:43 +0300

    Add ability to check BookGenre existence

--- HomeLibProt.Domain/DataAccess/BookGenres.cs
+++ HomeLibProt.Domain/DataAccess/BookGenres.cs
@@ -6,6 +6,13 @@ namespace HomeLibProt.Domain.DataAccess;
 
 public record BookGenreParam(long BookId, long GenreId);
 
+public enum BookGenreCheckResult {
+    AllExsists = 1,
+    OnlyBook,
+    OnlyGenre,
+    NoRecords
+}
+
 public static class BookGenres {
     public static async Task InsertBookGenresAsync(DbConnection connection, BookGenreParam[] bookGenreParams) {
         var sql =
@@ -18,5 +25,24 @@ values (@BookId, @GenreId)
         foreach (var bgp in bookGenreParams) {
             var _ = await connection.ExecuteAsync(sql, bgp);
         }
+    }
+
+    public static async Task<BookGenreCheckResult> CheckIfBookGenreExistsAsync(DbConnection connection, BookGenreParam bookGenreParam) {
+        var sql =
+            @"
+with
+    Book as (select max(1) as BookExsists from Books where Id = @BookId),
+    Genre as (select max(1) as GenreExsists from Genres where Id = @GenreId)
+select
+    case
+        when Book.BookExsists and Genre.GenreExsists then 1
+        when Book.BookExsists then 2
+        when Genre.GenreExsists then 3
+        else 4
+    end as Result
+from Book, Genre
+";
+
+        return await connection.QuerySingleAsync<BookGenreCheckResult>(sql, new { BookId = bookGenreParam.BookId, GenreId = bookGenreParam.GenreId });
     }
 }
