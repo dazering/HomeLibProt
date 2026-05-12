68a2f80979d2339a8475ec0377abdb5974e5007f
dazering <dazering@yandex.ru>
2026-05-12 20:07:49 +0300

    Add ability to check Authorships existence - tests

--- HomeLibProt.Domain.Tests/DataAccess/TestAuthorships.cs
+++ HomeLibProt.Domain.Tests/DataAccess/TestAuthorships.cs
@@ -45,4 +45,40 @@ public class TestAuthorships {
 
         Assert.That(actual, Is.EqualTo(expected).AsCollection);
     }
+
+    [Test]
+    [TestCase(1, 1, AuthorshipsCheckResult.AllExsists)]
+    [TestCase(1, 0, AuthorshipsCheckResult.OnlyBook)]
+    [TestCase(0, 1, AuthorshipsCheckResult.OnlyAuthor)]
+    [TestCase(0, 0, AuthorshipsCheckResult.NoRecords)]
+    public async Task TestCheckIfAuthorshipsExistsAsync(long bookId, long authorId, AuthorshipsCheckResult expected) {
+        var actual = await TestUtils.UseTestDatabase(
+            async (connection) => await DbStructure.CreateImportInpxStructure(connection, true),
+            async (connection) => {
+                await ConnectionUtils.DoInTransactionAsync(connection, async (c) => {
+                    var archiveId = await ArchiveUtils.Create(c, "archive1.zip");
+
+                    var langId = await LanguageUtils.Create(c, "Lang 1");
+
+                    await BookUtils.Create(c,
+                                           title: "Title1",
+                                           fileName: "File1",
+                                           size: 1,
+                                           libId: "File1",
+                                           deleted: false,
+                                           extension: "fb2",
+                                           date: "2025-11-07",
+                                           archiveId: archiveId,
+                                           libRate: 0,
+                                           languageId: langId);
+                    await AuthorUtils.Create(connection, fullName: "A A A", lastName: "A", firstName: "A", middleName: "A");
+                });
+
+                return await ConnectionUtils.DoInTransactionAsync(connection, async (c) => {
+                    return await Authorships.CheckIfAuthorshipsExistsAsync(c, new AuthorshipParam(BookId: bookId, AuthorId: authorId));
+                });
+            });
+
+        Assert.That(actual, Is.EqualTo(expected).AsCollection);
+    }
 }
