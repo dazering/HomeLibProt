c311ce1279d6a35b2953471436a2226078cb6434
dazering <dazering@yandex.ru>
2026-05-12 20:35:04 +0300

    Add ability to check BookSeries existence

--- HomeLibProt.Domain/DataAccess/BookSeries.cs
+++ HomeLibProt.Domain/DataAccess/BookSeries.cs
@@ -6,6 +6,13 @@ namespace HomeLibProt.Domain.DataAccess;
 
 public record BookSeriesParam(long BookId, long SeriesId, long SeriesNumber);
 
+public enum BookSeriesCheckResult {
+    AllExsists = 1,
+    OnlyBook,
+    OnlySeries,
+    NoRecords
+}
+
 public static class BookSeries {
     public static async Task InsertBookSeriesAsync(DbConnection connection, BookSeriesParam bookSeriesParam) {
         var sql =
@@ -15,5 +22,24 @@ BookSeries (BookId, SeriesId, SeriesNumber)
 values (@BookId, @SeriesId, @SeriesNumber)
 ";
         await connection.ExecuteAsync(sql, bookSeriesParam);
+    }
+
+    public static async Task<BookSeriesCheckResult> CheckIfBookSeriesExistsAsync(DbConnection connection, BookSeriesParam bookSeriesParam) {
+        var sql =
+            @"
+with
+    Book as (select max(1) as BookExsists from Books where Id = @BookId),
+    SeriesResult as (select max(1) as SeriesExsists from Series where Id = @SeriesId)
+select
+    case
+        when Book.BookExsists and SeriesResult.SeriesExsists then 1
+        when Book.BookExsists then 2
+        when SeriesResult.SeriesExsists then 3
+        else 4
+    end as Result
+from Book, SeriesResult
+";
+
+        return await connection.QuerySingleAsync<BookSeriesCheckResult>(sql, new { BookId = bookSeriesParam.BookId, SeriesId = bookSeriesParam.SeriesId });
     }
 }
