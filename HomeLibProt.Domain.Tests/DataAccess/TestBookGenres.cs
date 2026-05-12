408871eb7d2983c9f58751450e37c1e3d847b788
dazering <dazering@yandex.ru>
2026-05-12 20:27:49 +0300

    Add ability to check BookGenre existence - tests

--- HomeLibProt.Domain.Tests/DataAccess/TestBookGenres.cs
+++ HomeLibProt.Domain.Tests/DataAccess/TestBookGenres.cs
@@ -46,4 +46,40 @@ public class TestBookGenres {
 
         Assert.That(actual, Is.EqualTo(expected).AsCollection);
     }
+
+    [Test]
+    [TestCase(1, 1, BookGenreCheckResult.AllExsists)]
+    [TestCase(1, 0, BookGenreCheckResult.OnlyBook)]
+    [TestCase(0, 1, BookGenreCheckResult.OnlyGenre)]
+    [TestCase(0, 0, BookGenreCheckResult.NoRecords)]
+    public async Task TestCheckIfBookGenreExistsAsync(long bookId, long genreId, BookGenreCheckResult expected) {
+        var actual = await TestUtils.UseTestDatabase(
+            async (connection) => await DbStructure.CreateImportInpxStructure(connection, true),
+            async (connection) => {
+                await ConnectionUtils.DoInTransactionAsync(connection, async (c) => {
+                    var archiveId = await ArchiveUtils.Create(c, "archive1.zip");
+
+                    var langId = await LanguageUtils.Create(c, "Lang 1");
+
+                    await BookUtils.Create(c,
+                                           title: "Title1",
+                                           fileName: "File1",
+                                           size: 1,
+                                           libId: "File1",
+                                           deleted: false,
+                                           extension: "fb2",
+                                           date: "2025-11-07",
+                                           archiveId: archiveId,
+                                           libRate: 0,
+                                           languageId: langId);
+                    await GenreUtils.Create(c, key: "genre1", name: "Genre 1");
+                });
+
+                return await ConnectionUtils.DoInTransactionAsync(connection, async (c) => {
+                    return await BookGenres.CheckIfBookGenreExistsAsync(c, new BookGenreParam(BookId: bookId, GenreId: genreId));
+                });
+            });
+
+        Assert.That(actual, Is.EqualTo(expected).AsCollection);
+    }
 }
