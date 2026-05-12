a3fae7573bbaff34d480d33b356f0073f9f7e245
dazering <dazering@yandex.ru>
2026-05-12 20:45:24 +0300

    Add ability to check BookSeries existence - tests

--- HomeLibProt.Domain.Tests/DataAccess/TestBookSeries.cs
+++ HomeLibProt.Domain.Tests/DataAccess/TestBookSeries.cs
@@ -46,4 +46,40 @@ public class TestBookSeries {
 
         Assert.That(actual, Is.EqualTo(expected).AsCollection);
     }
+
+    [Test]
+    [TestCase(1, 1, BookSeriesCheckResult.AllExsists)]
+    [TestCase(1, 0, BookSeriesCheckResult.OnlyBook)]
+    [TestCase(0, 1, BookSeriesCheckResult.OnlySeries)]
+    [TestCase(0, 0, BookSeriesCheckResult.NoRecords)]
+    public async Task TestCheckIfCheckIfBookSeriesExistsAsync(long bookId, long seriesId, BookSeriesCheckResult expected) {
+        var actual = await TestUtils.UseTestDatabase(
+            async (connection) => await DbStructure.CreateImportInpxStructure(connection, true),
+            async (connection) => {
+                await ConnectionUtils.DoInTransactionAsync(connection, async (c) => {
+                    var archiveId = await ArchiveUtils.Create(c, "archive1.zip");
+
+                    var langId = await LanguageUtils.Create(c, "Lang 1");
+
+                    await BookUtils.Create(c,
+                                           title: "Title1",
+                                           fileName: "File1",
+                                           size: 1,
+                                           libId: "File1",
+                                           deleted: false,
+                                           extension: "fb2",
+                                           date: "2025-11-07",
+                                           archiveId: archiveId,
+                                           libRate: 0,
+                                           languageId: langId);
+                    await SeriesUtils.Create(c, name: "Series 1");
+                });
+
+                return await ConnectionUtils.DoInTransactionAsync(connection, async (c) => {
+                    return await BookSeries.CheckIfBookSeriesExistsAsync(c, new BookSeriesParam(BookId: bookId, SeriesId: seriesId, 0));
+                });
+            });
+
+        Assert.That(actual, Is.EqualTo(expected).AsCollection);
+    }
 }
