d7a91c34b5bb9afbb2e3315fd9590ae02213a067
dazering <dazering@yandex.ru>
2026-05-25 19:46:07 +0300

    Add GenreListLine

---
+++ HomeLibProt.CollectionImporter/GenreListLine.fs
@@ -0,0 +1,29 @@
+module HomeLibProt.CollectionImporter.GenreListLine
+
+open System.Text.RegularExpressions
+
+module private GenreInpLineRegExFields =
+    let public key = "key"
+    let public name = "name"
+
+type GenreListLine = { Key: string; Name: string }
+
+let private regExPattern =
+    $"(?<{GenreInpLineRegExFields.key}>[^\x04]*)\x04(?<{GenreInpLineRegExFields.name}>[^\x04]*)"
+
+let getRegEx () =
+    new Regex(regExPattern, RegexOptions.Compiled)
+
+let parseGenreListLine (regEx: Regex) (line: string) : GroupCollection =
+    let regExMatch = regEx.Match(line)
+
+    if regExMatch.Success then
+        regExMatch.Groups
+    else
+        failwith $"Unsupported genre line format: \"{line}\""
+
+let private extractGroupValue (group: Group) : string = group.Value
+
+let getGenreLine (groups: GroupCollection) : GenreListLine =
+    { Key = groups.[GenreInpLineRegExFields.key] |> extractGroupValue
+      Name = groups.[GenreInpLineRegExFields.name] |> extractGroupValue }
