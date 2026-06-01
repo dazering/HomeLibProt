0bd96ad07322bb1680a4594f3ab42c525fadd4e6
dazering <dazering@yandex.ru>
2026-06-01 15:46:55 +0300

    Get rid of publish warning

--- HomeLibProt.CollectionManager/SqlDumps/SqlDumpsDownloader.fs
+++ HomeLibProt.CollectionManager/SqlDumps/SqlDumpsDownloader.fs
@@ -15,17 +15,19 @@ type SqlDumpDownloaderParameters =
 let downloadSqlDumpsFlibustaAsync (parameters: SqlDumpDownloaderParameters) : Task<unit> =
     task {
         let urisToDownload =
-            [| Uri $"{Flibusta.url}/{Flibusta.authors}", Flibusta.authors
-               Uri $"{Flibusta.url}/{Flibusta.authorships}", Flibusta.authorships
-               Uri $"{Flibusta.url}/{Flibusta.bookGenres}", Flibusta.bookGenres
-               Uri $"{Flibusta.url}/{Flibusta.books}", Flibusta.books
-               Uri $"{Flibusta.url}/{Flibusta.bookSeries}", Flibusta.bookSeries
-               Uri $"{Flibusta.url}/{Flibusta.genres}", Flibusta.genres
-               Uri $"{Flibusta.url}/{Flibusta.rates}", Flibusta.rates
-               Uri $"{Flibusta.url}/{Flibusta.series}", Flibusta.series |]
+            [| Flibusta.authors
+               Flibusta.authorships
+               Flibusta.bookGenres
+               Flibusta.books
+               Flibusta.bookSeries
+               Flibusta.genres
+               Flibusta.rates
+               Flibusta.series |]
 
-        for uri, fileName in urisToDownload do
+        for fileName in urisToDownload do
             parameters.ProgressReport $"Downloading: {fileName}"
+
+            let uri = Uri $"{Flibusta.url}/{fileName}"
 
             do!
                 FileDownloader.downloadFileAsync
@@ -40,17 +42,19 @@ let downloadSqlDumpsFlibustaAsync (parameters: SqlDumpDownloaderParameters) : Ta
 let downloadSqlDumpsLibrusecAsync (parameters: SqlDumpDownloaderParameters) =
     task {
         let urisToDownload =
-            [| Uri $"{Librusec.url}/{Librusec.authors}", Librusec.authors
-               Uri $"{Librusec.url}/{Librusec.authorships}", Librusec.authorships
-               Uri $"{Librusec.url}/{Librusec.bookGenres}", Librusec.bookGenres
-               Uri $"{Librusec.url}/{Librusec.books}", Librusec.books
-               Uri $"{Librusec.url}/{Librusec.bookSeries}", Librusec.bookSeries
-               Uri $"{Librusec.url}/{Librusec.genres}", Librusec.genres
-               Uri $"{Librusec.url}/{Librusec.rates}", Librusec.rates
-               Uri $"{Librusec.url}/{Librusec.series}", Librusec.series |]
+            [| Librusec.authors
+               Librusec.authorships
+               Librusec.bookGenres
+               Librusec.books
+               Librusec.bookSeries
+               Librusec.genres
+               Librusec.rates
+               Librusec.series |]
 
-        for uri, fileName in urisToDownload do
+        for fileName in urisToDownload do
             parameters.ProgressReport $"Downloading: {fileName}"
+
+            let uri = Uri $"{Librusec.url}/{fileName}"
 
             do!
                 FileDownloader.downloadFileAsync
